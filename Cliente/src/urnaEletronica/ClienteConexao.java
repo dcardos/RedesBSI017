b3457958b482c147b6cab84c7b4186e57e03e9e7
Danilo Guarnieri Cardoso <cardoso.nilo@gmail.com>
2017-06-16 23:19:01 -0300

    Cliente conexao

---
+++ Cliente/src/urnaEletronica/ClienteConexao.java
@@ -0,0 +1,54 @@
+package urnaEletronica;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+
+/**
+ * Created by Danilo on 16/06/2017.
+ */
+public class ClienteConexao {
+
+    public void conecta() throws IOException{
+        // TODO: adicionar IP do servidor
+        String server = "127.0.0.1";
+
+        // Converte String de mensagem para bytes codificacao padrao
+        byte[] byteBuffer = "888".getBytes();   // carregar candidatos
+
+        // recebe porta por argumento opcional, default é a porta 7
+        // TODO: adicionar porta do servidor
+        int servPort = 2299;
+
+        // Cria o socket que sera utilizado para se conectar ao servidor na porta especificada
+        Socket socket = new Socket(server, servPort);
+
+        System.out.println("Conectado ao servidor... enviando a mensagem");
+
+        InputStream in = socket.getInputStream();
+
+        OutputStream out = socket.getOutputStream();
+
+        out.write(byteBuffer); // Envia a mensagem codificada ao servidor
+
+        // Recebe a mesma mensagem de volta
+        int totalBytesRcvd = 0;
+
+        int bytesRcvd;
+
+        // TODO: saber quando parar de ler (qual resposta?)
+        while (totalBytesRcvd < byteBuffer.length) {
+
+            if ((bytesRcvd = in.read(byteBuffer, totalBytesRcvd, byteBuffer.length - totalBytesRcvd)) == -1)
+                throw new SocketException("Conexao fechada inesperadamente");
+
+            totalBytesRcvd += bytesRcvd;
+        }
+
+        System.out.println("Mensagem recebida: " + new String(byteBuffer));
+
+        socket.close();
+    }
+}
